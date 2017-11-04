299a76420d43958bb3104ba42c8fef3ea55c2564
unternehmen <unternehmen@users.noreply.github.com>
2017-11-04 11:12:04 -0400

    Basic map rendering with pytmx

--- arctia.py
+++ arctia.py
@@ -3,6 +3,8 @@ import pygame
 import atexit
 import sys
 import os
+import pytmx
+import math
 
 
 # Game constants
@@ -11,6 +13,27 @@ SCREEN_LOGICAL_HEIGHT = 240
 SCREEN_LOGICAL_DIMS = SCREEN_LOGICAL_WIDTH, SCREEN_LOGICAL_HEIGHT
 SCREEN_ZOOM = 2
 SCREEN_REAL_DIMS = tuple([x * SCREEN_ZOOM for x in SCREEN_LOGICAL_DIMS])
+MENU_WIDTH = 16
+
+
+class Stage:
+    def __init__(self):
+        self.tiled_map = \
+            pytmx.TiledMap(os.path.join('maps', 'tuxville.tmx'))
+
+    def draw(self, screen, tileset, camera_x, camera_y):
+        for layer_ref in self.tiled_map.visible_tile_layers:
+            layer = self.tiled_map.layers[layer_ref]
+            for x, y, img in layer.tiles():
+                virtual_screen.blit(tileset,
+                                    (x * 16 - camera_x + MENU_WIDTH,
+                                     y * 16 - camera_y + MENU_WIDTH),
+                                    img[1])
+
+    def get_player_start_pos(self):
+        player_start_obj = \
+            self.tiled_map.get_object_by_name('Player Start')
+        return player_start_obj.x, player_start_obj.y
 
 
 if __name__ == '__main__':
@@ -21,8 +44,16 @@ if __name__ == '__main__':
     scaled_screen = pygame.Surface(SCREEN_REAL_DIMS)
 
     pygame.mixer.music.load(os.path.join('music', 'nescape.ogg'))
-    pygame.mixer.music.play(loops=-1)
+    tileset = pygame.image.load(os.path.join('gfx', 'tileset.png'))
+    stage = Stage()
 
+    player_start_x, player_start_y = stage.get_player_start_pos()
+    camera_x = player_start_x + 8 \
+               - math.floor(SCREEN_LOGICAL_WIDTH / 2.0)
+    camera_y = player_start_y + 8 \
+               - math.floor(SCREEN_LOGICAL_HEIGHT / 2.0)
+
+    pygame.mixer.music.play(loops=-1)
     clock = pygame.time.Clock()
     while True:
         # Handle user input.
@@ -32,6 +63,7 @@ if __name__ == '__main__':
 
         # Draw everything.
         virtual_screen.fill((255, 255, 255))
+        stage.draw(virtual_screen, tileset, camera_x, camera_y)
 
         # Scale and draw onto the real screen.
         pygame.transform.scale(virtual_screen,
