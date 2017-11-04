b33a05e59f04d10361a4df4b93199f3211aa1b66
unternehmen <unternehmen@users.noreply.github.com>
2017-11-04 10:15:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A weekend-long burglary of the basic features of Dwarf Fortress.
+
+Tux and his friends were surprised when a huge ice chunk drifted
+into the nearby sea.  The chunk was so huge that some penguins packed
+their bags and decided to stake out a piece of the chunk to live on.
+The elders call this chunk "Arctia", and you are leading a colony
+of penguin settlers on this new territory.
---
+++ arctia.py
@@ -0,0 +1,39 @@
+import pygame
+import atexit
+import sys
+
+
+# Game constants
+SCREEN_LOGICAL_WIDTH = 256
+SCREEN_LOGICAL_HEIGHT = 240
+SCREEN_LOGICAL_DIMS = SCREEN_LOGICAL_WIDTH, SCREEN_LOGICAL_HEIGHT
+SCREEN_ZOOM = 2
+SCREEN_REAL_DIMS = tuple([x * SCREEN_ZOOM for x in SCREEN_LOGICAL_DIMS])
+
+
+if __name__ == '__main__':
+    pygame.init()
+    atexit.register(pygame.quit)
+    screen = pygame.display.set_mode(SCREEN_REAL_DIMS)
+    virtual_screen = pygame.Surface(SCREEN_LOGICAL_DIMS)
+    scaled_screen = pygame.Surface(SCREEN_REAL_DIMS)
+
+    clock = pygame.time.Clock()
+    while True:
+        # Handle user input.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Draw everything.
+        virtual_screen.fill((255, 255, 255))
+
+        # Scale and draw onto the real screen.
+        pygame.transform.scale(virtual_screen,
+                               SCREEN_REAL_DIMS,
+                               scaled_screen)
+        screen.blit(scaled_screen, (0, 0))
+        pygame.display.flip();
+
+        # Wait for the next frame.
+        clock.tick(25)
