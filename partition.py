c65a1d17e3c29cb23ada062a447efa1ed832cd7c
unternehmen <unternehmen@users.noreply.github.com>
2017-11-21 12:45:47 -0500

    partition now conforms to pylint

--- partition.py
+++ partition.py
@@ -1,3 +1,6 @@
+"""
+The partition module provides a way of finding tiles a unit can reach.
+"""
 from common import tile_is_solid
 
 def partition(stage, location):
@@ -14,24 +17,24 @@ def partition(stage, location):
         an m-by-n array of bools in which True means a location
         is reachable and False means the location is unreachable
     """
-    lx, ly = location
+    loc_x, loc_y = location
 
-    assert 0 <= lx < stage.width
-    assert 0 <= ly < stage.height
+    assert 0 <= loc_x < stage.width
+    assert 0 <= loc_y < stage.height
 
     reachable = [[False for x in range(stage.width)]
                  for y in range(stage.height)]
-    reachable[ly][lx] = True
+    reachable[loc_y][loc_x] = True
 
     directions = [
         (0, -1), (0, 1), (-1, 0), (1, 0),
         (-1, -1), (1, -1), (-1, 1), (1, 1)
     ]
 
-    fringe = [(lx, ly)]
+    fringe = [(loc_x, loc_y)]
 
     while True:
-        if len(fringe) == 0:
+        if not fringe:
             break
 
         new_fringe = []
@@ -40,14 +43,14 @@ def partition(stage, location):
             if not tile_is_solid(stage.get_tile_at(x, y)) \
                or (x, y) == location:
                 for dx, dy in directions:
-                    nx = dx + x
-                    ny = dy + y
+                    neighbor_x = dx + x
+                    neighbor_y = dy + y
 
-                    if 0 <= nx < stage.width \
-                       and 0 <= ny < stage.height \
-                       and not reachable[ny][nx]:
-                        new_fringe.append((nx, ny))
-                        reachable[ny][nx] = True
+                    if 0 <= neighbor_x < stage.width \
+                       and 0 <= neighbor_y < stage.height \
+                       and not reachable[neighbor_y][neighbor_x]:
+                        new_fringe.append((neighbor_x, neighbor_y))
+                        reachable[neighbor_y][neighbor_x] = True
 
         fringe = new_fringe
 
