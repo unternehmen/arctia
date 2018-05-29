1e1c8d6e71e7330a6dcd8f3e1ee0535acb1c722f
unternehmen <unternehmen@users.noreply.github.com>
2018-05-29 14:39:28 -0400

    Add nose requirement for testing

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+requirements = ['pygame==1.9.3', 'pytmx==3.21.5']
+
 setup(
     name='arctia',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=['pygame==1.9.3', 'pytmx==3.21.5'],
+    install_requires=requirements,
+    tests_require=['nose==1.3.7'] + requirements,
     entry_points={
         'console_scripts': [
             'arctia = arctia:main'
